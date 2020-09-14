562ae7c0a7dee017e3e5f73a1894d8353857b0e5
Cameron Shimmin <cameronshimmin@Camerons-Mac-mini.local>
2020-09-14 15:39:24 -0700

    single room

--- src/context.js
+++ src/context.js
@@ -13,6 +13,7 @@ class RoomProvider extends Component {
   componentDidMount() {
     // this.getData
     let rooms = this.formatData(items);
+    console.log(rooms);
     let featuredRooms = rooms.filter((room) => room.featured === true);
     this.setState({
       rooms,
@@ -30,9 +31,14 @@ class RoomProvider extends Component {
     });
     return tempItems;
   }
+  getRoom = (slug) => {
+    let tempRooms = [...this.state.rooms];
+    const room = tempRooms.find((room) => room.slug === slug);
+    return room;
+  };
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state }}>
+      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
         {this.props.children}
       </RoomContext.Provider>
     );
