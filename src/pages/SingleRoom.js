63237959f536c734d168fe32ac8f147eb0ec973b
Cameron Shimmin <cameronshimmin@Camerons-Mac-mini.local>
2020-09-15 17:59:37 -0700

    fixes

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import defaultBcg from '../images/room-1.jpeg';
-import Hero from '../components/Hero';
+// import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
 import { RoomContext } from '../context';
@@ -13,7 +13,6 @@ export default class SingleRoom extends Component {
     this.state = { slug: this.props.match.params.slug, defaultBcg };
   }
   static contextType = RoomContext;
-  // componentDidMount() {}
   render() {
     const { getRoom } = this.context;
     const room = getRoom(this.state.slug);
@@ -62,7 +61,7 @@ export default class SingleRoom extends Component {
             <article className='info'>
               <h3>info</h3>
               <h6>price : ${price}</h6>
-              <h6>size : ${size}SQFT</h6>
+              <h6>size : ${size} SQFT</h6>
               <h6>
                 max capacity :{' '}
                 {capacity > 1 ? `${capacity} people` : `${capacity} person`}
